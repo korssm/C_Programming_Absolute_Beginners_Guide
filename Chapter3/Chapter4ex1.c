a2b805550cfed1eda33c120d83e29743f00b3cc2
serg <s7263808@yandex.ru>
2023-05-19 15:21:10 +0300

    Создан файл Chapter4ex1.c

---
+++ Chapter3/Chapter4ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+/* Эти три строки демонстрируют использование
+самых популярных управляющих последовательностей 
+*/
+    printf("Column A\tColumn B\tColumn C");
+    printf("\nMy Computer\'s Beep Sounds Like This:\a!\n");
+    printf("\"Letz\bs fix that typo and then show thebackslash ");
+    printf("character \\\" she said\n");
+
+    return 0;
+}
